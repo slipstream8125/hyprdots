1f83c6e7e71b40af1441bf396621c9c585852e47
Slipstream <hatkardhruv@gmail.com>
2024-04-06 18:03:19 +0530

    Changed some stuff and added hypridle

--- waybar/scripts/power-menu.sh
+++ waybar/scripts/power-menu.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-entries="Logout Suspend Reboot Shutdown"
+entries="Lock Logout Suspend Reboot Shutdown"
 
 selected=$(printf '%s\n' $entries | wofi --conf=$HOME/.config/wofi/config.power --style=$HOME/.config/wofi/style.widgets.css | awk '{print tolower($1)}')
 
 case $selected in
+  lock)
+    exec hyprlock;;
   logout)
     swaymsg exit;;
   suspend)
