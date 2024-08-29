0da0547dc21d819809877f5686d648d5e2d32ced
Slipstream <hatkardhruv@gmail.com>
2024-08-30 00:01:25 +0530

    Updots

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -50,7 +50,7 @@ case $1 in
         selected=$( find "$HOME/hyprdots/wallpaper" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort -R | while read rfile
         do
             echo -en "$rfile\x00icon\x1f$HOME/wallpaper/${rfile}\n"
-        done | wofi -dmenu -i -no-show-icons)
+        done | wofi -dmenu -i --no-show-icons)
         if [ ! "$selected" ]; then
             echo "No wallpaper selected"
             exit
