8570fcf8fc995d07183fcb3e63f4b49b1e87c745
Slipstream <hatkardhruv@gmail.com>
2025-10-03 18:13:51 +0530

    Updots

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,18 @@
-status is-interactive; and begin
+# status is-interactive; and begin
     # set fish_tmux_autostart true
-end
+# end
 source ~/.local/share/cachyos-fish-config/cachyos-config.fish
 source $HOME/.config/fish/aliases.fish
 source $HOME/.config/fish/profile.fish
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
 export LC_ALL="en_US.UTF-8"
-pyenv init - fish | source
+# oh-my-posh init fish --config 'https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/space.omp.json' | source
+# pyenv init - fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/slipstream/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
